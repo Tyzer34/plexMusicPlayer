bfa5206a01250f295351c31a475a895ab25937ec
HertzRefArch <tdelesio@hertz.com>
2017-01-11 16:44:24 -0500

    adding a print to test the new branch

--- plexmusicplayer/methods.py
+++ plexmusicplayer/methods.py
@@ -77,6 +77,8 @@ def parseArtistJson(json_obj):
     print('parseArtistJson')
     playlist = []
 
+    print('json_obj=')
+    print(json_obj)
     directory = json_obj['MediaContainer']['Directory']
     print('directory=')
     print(directory)
