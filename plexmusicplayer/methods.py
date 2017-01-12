919e76a28ea3bdaa341ea02d2669af0eb3573b76
HertzRefArch <tdelesio@hertz.com>
2017-01-12 12:47:01 -0500

    trying to convert numbers back to words

--- plexmusicplayer/methods.py
+++ plexmusicplayer/methods.py
@@ -7,8 +7,49 @@ import json
 base_url = environ['PLEX_URL']
 plex_token = "X-Plex-Token=" + environ['PLEX_TOKEN']
 
+num2words1 = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
+            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
+            11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
+            15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
+num2words2 = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
 # --------------------------------------------------------------------------------------------
 # Plex Music Player - Methods
+
+def findNumberInQuery(input):
+    words = input.split()
+    response = ""
+    for word in  words: 
+        value = ""
+        if hasNumbers(word):
+            value = numberToWords(word)
+        else:     
+            value = word
+
+        response = response + value
+    return response
+
+
+
+def numberToWords(Number):
+    if 1 <= Number < 19:
+        return num2words1[Number]
+    elif 20 <= Number <= 99:
+        tens, below_ten = divmod(Number, 10)
+        return num2words2[tens - 2] + '-' + num2words1[below_ten]
+    else:
+        print("Number out of range")
+        return Number
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+def rebuildQueryWithoutSpaces(json_obj):
+    if json_obj:
+        query.replace(" ","")
+        print(query)
+        searchQueryUrl = base_url + "/search?query=" + query + "&" + plex_token + "&type=" + mediaType.value
+        json_obj = getJsonFromPlex(searchQueryUrl)
+    return json_obj
 
 def getStreamUrl(sub_url):
     global base_url, plex_token
@@ -110,13 +151,6 @@ def processQuery(query, mediaType):
     searchQueryUrl = base_url + "/search?query=" + query + "&" + plex_token + "&type=" + mediaType.value
     json_obj = getJsonFromPlex(searchQueryUrl)
 
-    if json_obj:
-        query.replace(" ","")
-        print(query)
-        searchQueryUrl = base_url + "/search?query=" + query + "&" + plex_token + "&type=" + mediaType.value
-        json_obj = getJsonFromPlex(searchQueryUrl)
-    else:
-        print('found value')
 
     playlist = []
     try:
@@ -128,6 +162,12 @@ def processQuery(query, mediaType):
             album, artist, server, playlist = parseAlbumJson(json_obj)
             speech = "Playing " + album + " by " + artist + " from " + server + "."
         elif (mediaType == MediaType.Artist):
+            if hasNumbers(query):
+                query = findNumberInQuery(query)
+                searchQueryUrl = base_url + "/search?query=" + query + "&" + plex_token + "&type=" + mediaType.value
+                json_obj = getJsonFromPlex(searchQueryUrl)
+
+            json_obj = rebuildQueryWithoutSpaces(json_obj)
             artist, server, playlist = parseArtistJson(json_obj)
             speech = "Playing " + artist + " from " + server + "."
         return speech, playlist
