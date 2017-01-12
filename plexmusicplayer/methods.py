0a444e3c08351a3454167daa473ea646c8534dcf
HertzRefArch <tdelesio@hertz.com>
2017-01-12 12:58:13 -0500

    trying to convert numbers back to words

--- plexmusicplayer/methods.py
+++ plexmusicplayer/methods.py
@@ -35,7 +35,7 @@ def findNumberInQuery(input):
 
 
 def numberToWords(Number):
-    print(Number)
+    print(":"+Number+":")
     if 1 <= Number < 19:
         print('Number='+Number)
         print(num2words1[Number])
